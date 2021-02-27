4dd5a7ca66548f618b27cceb3df46ec36ce7c301
blee99-umgc <77966226+blee99-umgc@users.noreply.github.com>
2021-02-27 15:30:04 -0500

    Adding files for sliding menu.

--- formbot_app/lib/main.dart
+++ formbot_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:formbot_app/app_drawer.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -68,6 +70,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      // Select the drawer that is needed
+      //  authDrawer - Authorized Users
+      //  unauthDrawer - Unauthorized Users
+      endDrawer: authDrawer(),
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
