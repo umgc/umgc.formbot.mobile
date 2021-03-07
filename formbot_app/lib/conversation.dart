f0841a8eab4e623a62590b778a141b9e231134ae
kmmansour2 <kmansour@student.umgc.edu>
2021-03-07 11:45:45 -0500

    conversation page

--- formbot_app/lib/conversation.dart
+++ formbot_app/lib/conversation.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:dialog_flowtter/dialog_flowtter.dart';
 
-import 'widgets/app_drawer.dart';
+import 'app_body.dart';
 
-class ConversationPage extends StatelessWidget {
+/*class ConversationPage extends StatelessWidget {
   static const String routeName = 'conversation';
 
   @override
@@ -26,4 +27,101 @@ class ConversationPage extends StatelessWidget {
         endDrawer: AuthDrawer(),
         body: Center(child: Text("Conversation")));
   }
+}*/
+
+class ConversationPage extends StatefulWidget {
+  static const String routeName = 'conversation';
+  ConversationPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ConversationPageState createState() => _ConversationPageState();
 }
+
+class _ConversationPageState extends State<ConversationPage> {
+  DialogFlowtter dialogFlowtter;
+  final TextEditingController _controller = TextEditingController();
+
+  List<Map<String, dynamic>> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    DialogFlowtter.fromFile().then((instance) => dialogFlowtter = instance);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Expanded(child: AppBody(messages: messages)),
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 5,
+            ),
+            color: Colors.blue,
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                IconButton(
+                  color: Colors.white,
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    sendMessage(_controller.text);
+                    _controller.clear();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void sendMessage(String text) async {
+    if (text.isEmpty) return;
+    setState(() {
+      addMessage(
+        Message(text: DialogText(text: [text])),
+        true,
+      );
+    });
+
+    dialogFlowtter.projectId = "formscriber2-gdoo";
+
+    DetectIntentResponse response = await dialogFlowtter.detectIntent(
+      queryInput: QueryInput(text: TextInput(text: text)),
+    );
+
+    if (response.message == null) return;
+    setState(() {
+      addMessage(response.message);
+    });
+  }
+
+  void addMessage(Message message, [bool isUserMessage]) {
+    messages.add({
+      'message': message,
+      'isUserMessage': isUserMessage ?? false,
+    });
+  }
+
+  @override
+  void dispose() {
+    dialogFlowtter.dispose();
+    super.dispose();
+  }
+}
+
