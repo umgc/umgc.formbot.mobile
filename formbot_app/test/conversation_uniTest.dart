79a388bb505d5e46fc7d5403bf8022f4dad05e02
Arnaud Tako <arnaudtako@arnauds-mbp.myfiosgateway.com>
2021-03-14 15:59:11 -0400

    added unit test cases files

---
+++ formbot_app/test/conversation_uniTest.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:formbot_app/conversation.dart';
+
+void main() {
+  test('Testing conversation message with null', ()  {
+      //Arrange
+      var answer=false;
+
+      //Act
+      ConversationPage().createState().sendMessage(null);
+      
+      //Assert
+      expect(answer, null);
+
+
+      
+      });
+
+      // pass message as a test case
+      test('Testing conversation message with message ', ()  {
+      //Arrange
+      bool answer=false;
+
+      //Act
+      var obj= ConversationPage();
+      obj.createState().sendMessage("Hello");
+      
+      //Assert
+      expect(answer,"Hello");
+
+
+     
+      });
+    }
+    
