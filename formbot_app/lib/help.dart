b3d4e495211faba566477b1492a16dbe6b179556
Alex <Alex.m.dattilio@gmail.com>
2021-04-03 12:46:09 -0400

    Updated help.dart

--- formbot_app/lib/help.dart
+++ formbot_app/lib/help.dart
@@ -4,46 +4,50 @@ University of Maryland is not responsible for any shortcomings and the user is s
 
 import 'package:flutter/material.dart';
 import 'widgets/app_drawer.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class HelpPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HelpPage extends StatefulWidget {
   static const String routeName = 'help';
 
   @override
-  Widget build(BuildContext context) {
-
-    Future<bool> _willPopCallback() async {
-      Navigator.pushNamedAndRemoveUntil(
-          context,
-          routeName,
-          ModalRoute.withName("/")
-      );
-      return false; // return true if the route to be popped
-    }
-
-    new WillPopScope(child: new Scaffold(), onWillPop: _willPopCallback);
-
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text("Help"),
-        backgroundColor: Color(0xFF007fbc),
-
-      ),
-      endDrawer: AuthDrawer(),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text("Help"),
-            ElevatedButton(
-              key: Key('help_back_btn'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Back'),
-              style: ElevatedButton.styleFrom(primary: Color(0xFF007fbc)),
-            ),
-          ]
-        )
-      )
-    );
+  State<StatefulWidget> createState() {
+    return _HelpPageState();
   }
 }
+
+class _HelpPageState extends State<HelpPage> {
+  //
+  WebViewController _webViewController;
+  String filePath = 'assets/files/help.html';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Help')),
+        body: WebView(
+          initialUrl: 'https://duchesam.github.io/HelpPage/help.html',
+          javascriptMode: JavascriptMode.unrestricted,
+
+        )
+    );
+  }
+
+  _loadHtmlFromAssets() async {
+    String fileHtmlContents = await rootBundle.loadString(filePath);
+    _webViewController.loadUrl(Uri.dataFromString(fileHtmlContents,
+        mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
+        .toString());
+
+    _webViewController.loadUrl("http://google.com");
+
+
+  }
+}
+
